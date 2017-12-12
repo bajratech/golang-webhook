760d51aae5421859edf8ac9c6d48aaa03ea6f0f5
pravinray <pravin@bajratechnologies.com>
2017-12-12 20:04:05 +0545

    pod name improved

--- src/github.com/bajratech/webhook/start.sh
+++ src/github.com/bajratech/webhook/start.sh
@@ -35,7 +35,7 @@ StartPod(){
           ;;
        "redis-server") kubectl create -f /root/pods/redis-pod.yml
           ;;
-       "angular") kubectl create -f /root/pods/angular-pod.yml
+       "hawk-angular") kubectl create -f /root/pods/angular-pod.yml
           ;;
        "pagerank") kubectl create -f /root/pods/pagerank-pod.yml
           ;;
@@ -79,7 +79,7 @@ deletePod(){
           ;;
        "redis-server") kubectl delete rc redis-rc
           ;;
-       "angular") kubectl delete rc site-granny-angular
+       "hawk-angular") kubectl delete rc site-granny-angular
           ;;
        "pagerank") kubectl delete rc site-granny-pagerank
           ;;
